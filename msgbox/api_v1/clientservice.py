3b23e4175a1bbf1d93a5104fb6a5913130946c99
pengchant <13961826751@163.com>
2020-01-17 14:43:41 +0800

    后端与客户端简单通信功能完成

--- msgbox/api_v1/clientservice.py
+++ msgbox/api_v1/clientservice.py
@@ -7,12 +7,13 @@ from flask import request, jsonify
 from msgbox import db_session
 from msgbox.api_v1 import api
 from msgbox.models import User, SystemMessage
-from msgbox.utils.common import json_param_required
+from msgbox.utils.common import json_param_required, token_required
 from msgbox.utils.messge_util import get_usr_msg
 from msgbox.utils.response_code import RET
 
 
 @api.route("/client_update_msg", methods=['POST', 'GET'])
+@token_required
 @json_param_required
 def clientFetchData():
     """
@@ -46,3 +47,24 @@ def clientFetchData():
     except Exception as e:
         print(e)
         return jsonify(re_code=RET.DBERR, msg="更新数据失败，请稍后重试")
+
+
+@api.route("/update_hasread", methods=['POST', 'GET'])
+@token_required
+def update_hasread():
+    """
+    更新消息的状态已被阅读
+    :return:
+    """
+    msgid = request.args.get("msgid")
+    if not msgid:
+        return jsonify(re_code=RET.PARAMERR, msg="提交参数有误")
+    msg = SystemMessage.query.filter(SystemMessage.id == msgid).first()
+    if msg is None:
+        return jsonify(re_code=RET.PARAMERR, msg="查无消息")
+    try:
+        msg.msg_status = "HAVE_READED"
+        db_session.commit()
+        return jsonify(re_code=RET.OK, msg="修改消息状态成功")
+    except Exception as e:
+        return jsonify(re_code=RET.DBERR, msg="更新失败，请稍重试")
